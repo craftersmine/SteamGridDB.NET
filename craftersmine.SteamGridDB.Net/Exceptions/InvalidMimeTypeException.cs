75b05602db628f6c40aabc1f940220a5f835335b
craftersmine <rodion.morozov.01@gmail.com>
2022-07-18 20:12:41 +0700

    Added Invalid MIME type exception

---
+++ craftersmine.SteamGridDB.Net/Exceptions/InvalidMimeTypeException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.SteamGridDBNet.Exceptions
+{
+    [Serializable]
+    public class InvalidMimeTypeException : Exception
+    {
+        public InvalidMimeTypeException()
+        {
+        }
+
+        public InvalidMimeTypeException(string message) : base(message)
+        {
+        }
+
+        public InvalidMimeTypeException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected InvalidMimeTypeException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
