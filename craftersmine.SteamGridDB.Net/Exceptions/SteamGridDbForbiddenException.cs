e0d717174b9d12d4ddedbe3e2a07ef8168e88c17
craftersmine <rodion.morozov.01@gmail.com>
2022-07-18 20:13:03 +0700

    Added SteamGridDB exceptions

---
+++ craftersmine.SteamGridDB.Net/Exceptions/SteamGridDbForbiddenException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.SteamGridDBNet.Exceptions
+{
+    public class SteamGridDbForbiddenException : SteamGridDbException
+    {
+        public SteamGridDbForbiddenException(string message) : base(message)
+        {
+
+        }
+    }
+}
